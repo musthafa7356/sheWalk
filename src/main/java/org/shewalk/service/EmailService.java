f95f7856f1fac369957f642e8ed62d7d5a9dfc55
Musthafa C <muthumusthafa7356@gmail.com>
2026-02-28 00:08:07 +0530

    Updated async configurations

--- src/main/java/org/shewalk/service/EmailService.java
+++ src/main/java/org/shewalk/service/EmailService.java
@@ -3,6 +3,7 @@ package org.shewalk.service;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    @Async
     public void sendTrackingLink(String toEmail, String link) {
 
         try {
--- src/main/java/org/shewalk/sheWalkApplication.java
+++ src/main/java/org/shewalk/sheWalkApplication.java
@@ -2,8 +2,10 @@ package org.shewalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class sheWalkApplication {
     public static void main(String[] args) {
         SpringApplication.run(sheWalkApplication.class, args);
